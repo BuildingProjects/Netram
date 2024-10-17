9177ad2c2ef0bdce326fc590b477aab3038bffab
Ankur-Dwivedi22 <ankur.2201005cs@iiitbh.ac.in>
2024-10-17 14:41:45 +0530

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { useState } from 'react';
 
 const Navbar = () => {
@@ -56,7 +57,34 @@ const Navbar = () => {
         </div>
       )}
     </nav>
+=======
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HeroSection from "./components/HeroSection";
+import ProductPage from "./components/ProductPage";
+import Navbar from "./components/Navbar";
+import FeatureSection from "./components/FeatureSection";
+import HomePage from "./components/HomePage";
+import About from "./components/About ";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/product' element={<ProductPage />} />
+          {/* <Route path='/about' element={<About />} /> */}
+        </Routes>
+      </Router>
+      <div id='contact'>
+        <Contact />
+      </div>
+    </>
+>>>>>>> Stashed changes
   );
-};
+}
 
 export default Navbar;
